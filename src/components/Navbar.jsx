d9d25d8c2b85ee159221ce3adbc2c2d537783783
avinashrajputt1 <avinashsinghrathore1206@gmail.com>
2024-10-18 19:38:26 +0530

    up

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -30,7 +30,7 @@ const Navbar = () => {
         <ul className='list-none hidden sm:flex flex-row gap-10'>
           {navLinks.map((Link) => (
             <li key={Link.id} className={`${active===Link.title
-              ? "text-white"
+              ? "text-red"
               : "text-secondary"
             }
             hover:text-white text-[18px] font-medium cursor-pointer
