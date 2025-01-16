3aa6d3a3b0bb2d01f09c54ec583fa02f6e1182b1
avinashrajputt1 <avinashsinghrathore1206@gmail.com>
2025-01-16 20:46:45 +0530

    header

--- portfolio/src/components/Navbar/navbar.js
+++ portfolio/src/components/Navbar/navbar.js
@@ -1,16 +1,21 @@
 import React from "react";
 import "./navbar.css";
 import logo from "../../assets/logo.png";
+import { Link } from "react-scroll";
+import contactImg from "../../assets/contact.png";
 
 const Navbar = () => {
     return (
         <nav className="navbar">
             <img src={logo} alt="Logo" className="logo"/>
             <div className="desktop-menu">
-
+                <Link to="About" className="desktopMenuListItem" smooth={true} duration={500}>About</Link>
+                <Link to="Skills" className="desktopMenuListItem" smooth={true} duration={500}>Skills</Link>
+                <Link to="Project" className="desktopMenuListItem" smooth={true} duration={500}>Projects</Link>
+                <Link to="contact" className="desktopMenuListItem" smooth={true} duration={500}>Contact</Link>
             </div>
             <button className="desktopMenubtn">
-                <img src="" alt="" className="desktopMenuImg"/>Contact Me
+                <img src={contactImg} alt="" className="desktopMenuImg"/>Contact Me
             </button>
         </nav>
     );
